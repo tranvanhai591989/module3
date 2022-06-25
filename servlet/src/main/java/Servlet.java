54f6e47285f959b555fbbec8850c63271ddb770d
tranvanhai591989 <tranvanhai591989@gmail.com>
2022-06-25 20:28:46 +0700

    first commit

---
+++ servlet/src/main/java/Servlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@javax.servlet.annotation.WebServlet(name = "Servlet",urlPatterns = "/index")
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+    }
+}
